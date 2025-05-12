b01dea8e87efb36b356c821e25184146b1a2f993
wukx <wu_kaixia@qq.com>
2025-05-12 16:58:46 +0800

    Init

--- src/main/java/com/wukx/controller/TestController.java
+++ src/main/java/com/wukx/controller/TestController.java
@@ -1,5 +1,13 @@
 package com.wukx.controller;
 
+
 public class TestController {
     public final static String TEST_STRING = "test";
+
+    public TestController() {
+    }
+
+    public String toString() {
+        return "TestController{TEST_STRING = " + TEST_STRING + "}";
+    }
 }
